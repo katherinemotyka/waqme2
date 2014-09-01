e6f0d97d8ffbae193a74d62db173c7271cbea3c7
Jayesh Kavathiya <jayeshkavathiya@gmail.com>
2014-09-01 15:32:32 +0530

    Initial Commit

---
+++ Waqme/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Waqme
+//
+//  Created by Zeus_MAC on 01/09/14.
+//  Copyright (c) 2014 ZeusTechnocrats. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Waqme/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Waqme
+//
+//  Created by Zeus_MAC on 01/09/14.
+//  Copyright (c) 2014 ZeusTechnocrats. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
